12fe27207df03c88180fb681b68864ac53b9a2d0
shruthim <136746698+shruthi-mj@users.noreply.github.com>
2024-03-08 18:12:49 +0530

    Updating the comment

--- src/main/java/org/test/trout/traversal/PointerTreeTraversal.java
+++ src/main/java/org/test/trout/traversal/PointerTreeTraversal.java
@@ -10,15 +10,8 @@ import org.test.trout.model.Node;
 
 /**
  * 
- * The PointerTreeTraversal is an implementation of tree traversal using depth first search. As we traverse the nodes
- * we invoke rendering engine with node as an argument to take decision whether to render or not based on the generation
- * id.
- * As part of this algorithm, using a stack to keep reference of current state where we switched to next level, so once 
- * we traverse next level it would be possible to start from the node from where we left in current level.
- * 
- * Why not using the ref in head node of next level and using stack?
- * - Not sure whether the number of nodes in the tree are same as in Indirection table?
- * - Also ref has space of 8 bits, meaning the whole tree can only have 256 nodes.  
+ * The PointerTreeTraversal is an implementation of tree traversal uses the given head node for top level traversal.
+ * The used the indirection table to provide the head node for other levels.
  */
 public class PointerTreeTraversal {
 	
